01f0de6e64cf2374b5883b91510685836fffd5a4
catrionameriel <catriona.meriel@gmail.com>
2018-01-27 10:45:46 +0000

    food constructor created and tests passing

---
+++ food.js
@@ -0,0 +1,6 @@
+const Food = function(name, replenishmentValue) {
+ this.name = name;
+ this.replenishment = replenishmentValue;
+};
+
+module.exports = Food;
---
+++ specs/food_spec.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const Food = require('../food');
+
+describe('Food', function() {
+
+  let food;
+
+  beforeEach(function() {
+    food = new Food('Yoghurt', 20);
+  })
+
+  it('food has name', function() {
+    assert.strictEqual(food.name, 'Yoghurt');
+  })
+
+  it('food has replenishment value', function() {
+    assert.strictEqual(food.replenishment, 20);
+  })
+
+
+
+})
