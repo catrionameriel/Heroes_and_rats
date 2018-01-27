81b4493bc2239d2df6217a03b33c97993eca1846
catrionameriel <catriona.meriel@gmail.com>
2018-01-27 14:53:27 +0000

    can fight arch enemy test passing

--- specs/villain_spec.js
+++ specs/villain_spec.js
@@ -5,10 +5,12 @@ const Hero = require('../hero');
 describe('Villain', function() {
   let villain;
   let hero;
+  let hero2;
 
   beforeEach(function() {
     villain = new Villain('Frankenfurter', 'Theobold', 10);
     hero = new Hero('Theobold', 'Yoghurt', 'Invisibility');
+    hero2 = new Hero('Gertrudian', 'Irn Bru', 'Fireballs');
   })
 
   it('villain has name', function() {
@@ -21,6 +23,20 @@ describe('Villain', function() {
 
   it('villain has attack power', function() {
     assert.strictEqual(villain.attack, 10);
+  })
+
+  it('villain has health', function() {
+    assert.strictEqual(villain.health, 80);
+  })
+
+  it('villain can fight', function() {
+    villain.fight(hero2);
+    assert.strictEqual(hero2.health, 90);
+  })
+
+  it('villain can fight arch enemy', function() {
+    villain.fight(hero);
+    assert.strictEqual(hero.health, 89);
   })
 
 
--- villain.js
+++ villain.js
@@ -2,6 +2,18 @@ const Villain = function(name, archEnemy, attack) {
   this.name = name;
   this.archEnemy = archEnemy;
   this.attack = attack;
+  this.health = 80;
 }
+
+Villain.prototype.checkArchEnemy = function (hero) {
+  if (hero.name === this.archEnemy) {
+      this.attack += (this.attack * 0.1)
+  }
+};
+
+Villain.prototype.fight = function (hero) {
+  this.checkArchEnemy(hero);
+  hero.health -= this.attack;
+};
 
 module.exports = Villain;
